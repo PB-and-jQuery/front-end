ebddc38060afa9d088e734de55b7c890d85acdc1
John Cooley <johndavidcooley@gmail.com>
2016-04-16 18:26:37 -0400

    Added code to ballot.js

--- src/js/ballot.js
+++ src/js/ballot.js
@@ -2,14 +2,47 @@ import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
 import { Link, Router, Route, IndexRoute, hashHistory} from 'react-router';
 import SSF from 'react-simple-serial-form';
+import { ajax } from 'jquery';
 
 export default class Ballot extends Component {
+
+	constructor(...args) {
+		super(...args);
+
+		this.state = { candidates: [] };
+	}
+
+	componentWillMount() {
+		ajax('https://young-gorge-64909.herokuapp.com/candidate').then(candidates => {
+			this.setState({candidates})
+		})
+	}
+
+	candidateMapper(candidate) {
+		return (
+			<div>
+				<input type="radio" name="vote" value={`${candidate.name}     ${candidate.party}`} />
+			</div>
+		)
+	}
+
+
+	dataHandler(data) => {
+		console.log(data)
+		ajax('https://young-gorge-64909.herokuapp.com/candidate').then({
+			hashHistory.push('/dashboard');
+		})
+	}
+
+
 
 	render(){
 		return(
 			<div>
-				<h1>Ballot</h1>
-				<Link to="/dashboard">Done</Link>
+				<SSF onData={this.dataHandler}>
+					{candidates.map(::this.candidateMapper)}
+					<button>Submit Your Vote</button>
+				</SSF>
 			</div>
 		)
 	}
