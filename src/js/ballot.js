acabe7a9e12e1fd859dc017d30abc29ae6971990
John Cooley <johndavidcooley@gmail.com>
2016-04-16 20:04:06 -0400

    Edits to ballot file.

--- src/js/ballot.js
+++ src/js/ballot.js
@@ -9,12 +9,12 @@ export default class Ballot extends Component {
 	constructor(...args) {
 		super(...args);
 
-		this.state = { candidates: [] };
+		this.state = { candidate: [] };
 	}
 
 	componentWillMount() {
-		ajax('https://young-gorge-64909.herokuapp.com/candidate').then(candidates => {
-			this.setState({candidates})
+		ajax('https://young-gorge-64909.herokuapp.com/candidate').then(candidate => {
+			this.setState({candidate})
 		})
 	}
 
@@ -26,21 +26,40 @@ export default class Ballot extends Component {
 		)
 	}
 
-
-	dataHandler(data) => {
-		console.log(data)
-		ajax('https://young-gorge-64909.herokuapp.com/candidate').then({
+	dataHandler(vote){
+		ajax({
+			url: 'https://young-gorge-64909.herokuapp.com/votes',
+			type: 'POST',
+			data: vote,
+			cache: false,
+			dataType: 'json',
+		}).then(response => {
+			console.log('Vote Response-->', response);
 			hashHistory.push('/dashboard');
 		})
 	}
 
+	// dataHandler(data) => {
+	// 	console.log(data)
+	// 	ajax({
+	// 		url: 'https://young-gorge-64909.herokuapp.com/votes',
+	// 		type: 'POST',
+	// 		data: data,
+	// 		cache: false,
+	// 		dataType: 'json'
+	// 	}).then({
+	// 		hashHistory.push('/dashboard');
+	// 	})
+	// };
+
 
 
 	render(){
+		let { candidate } = this.state;
 		return(
 			<div>
 				<SSF onData={this.dataHandler}>
-					{candidates.map(::this.candidateMapper)}
+					{candidate.map(::this.candidateMapper)}
 					<button>Submit Your Vote</button>
 				</SSF>
 			</div>
---
+++ src/js/donut.js
@@ -0,0 +1,4 @@
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom';
+import { Link, Router, Route, IndexRoute, hashHistory} from 'react-router';
+import SSF from 'react-simple-serial-form';
